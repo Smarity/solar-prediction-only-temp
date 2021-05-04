d95e4e7a95051e7db263e83a8a4741490305b004
jabellido94 <jabellido1@gmail.com>
2021-05-04 09:36:58 +0200

    Script COR06 model (finished) Tests for COR06 model (passed)

--- cor06_svm.py
+++ cor06_svm.py
@@ -1,6 +1,9 @@
 import pickle
 import pandas as pd
+import numpy as np
+import StatsFunctions as stats
 from sklearn.svm import SVR
+from sklearn.preprocessing import StandardScaler
 
 class cor06_svm():
     """
@@ -10,7 +13,6 @@ class cor06_svm():
         ['tx', 'tn', 'ra', 'deltat', 'energyt', 'hormintx', 'tx_prev', 'tn_next', 'rs']
 
     """
-
     def __init__(self):
         # import model
         filename = "models/svm_cor06_Tx_Tn_Ra_deltaT_EnergyT_HorminTx_Tx_prev_Tn_next_Rs.sav"
@@ -18,7 +20,7 @@ class cor06_svm():
             self.model = pickle.load(file)
 
         # define required inputs
-        self.parameters = ['tx', 'tn', 'ra', 'deltaT', 'EnergyT', 'HorminTx', 'Tx_prev', 'Tn_next', 'Rs']
+        self.parameters = ['tx', 'tn', 'ra', 'delta_t', 'energyt', 'hormin_tx', 'tx_prev', 'tn_next', 'rs']
 
     def import_dataset(self, fileLocation, fileType):
         """
@@ -34,13 +36,51 @@ class cor06_svm():
         else:
             AssertionError('this fileType does not exit, use excel, csv and txt instead')
 
-        # filter by parameters
-        #self.dfData = self.dfData.filter(self.parameters)
+        # filter nan values
+        self.dfData.dropna(inplace=True)
+        self.dfData.reset_index(drop=True, inplace=True)
+        # filter the parameters
+        self.dfData = self.dfData.filter(self.parameters)
 
-    def predict(self):
-        pass
+    def getStandardDataTest(self):
+        """
+        Split data to train and test
+        """
+        # from training original dataset
+        mean_list = [24.42, 11.03, 28.78, 13.38, 402.95, 15.20, 24.42, 11.02]
+        std_list = [8.54, 6.26, 9.64, 4.578, 177.87, 1.78, 8.53, 6.27]
+
+        # we have the input data as x, and the output as y
+        self.x_test = np.array(self.dfData.iloc[:, :-1])
+        self.y_test = np.array(self.dfData.iloc[:, -1])
+
+        # standarization
+        scaler = StandardScaler()
+        scaler.mean_ = mean_list
+        scaler.scale_ = std_list
+
+        # x_train and x_test
+        self.x_test = np.array(scaler.transform(self.x_test))
+
+        return self.x_test, self.y_test
+
+    def predictValues(self):
+        self.y_pred = np.array(self.model.predict(self.x_test))
+        return self.y_pred
+
+    def statAnalysis(self):
+        rmse = stats.get_root_mean_square_error(self.y_test, self.y_pred)
+        rrmse = stats.get_root_mean_square_error(self.y_test, self.y_pred) / np.mean(self.y_test)
+        mbe = stats.get_mean_bias_error(self.y_test, self.y_pred)
+        r2 = stats.get_coefficient_of_determination(self.y_test, self.y_pred)
+        nse = stats.get_nash_suteliffe_efficiency(self.y_test, self.y_pred)
+
+        return rmse, rrmse, mbe, r2, nse
 
 if __name__ == '__main__':
     mlModel = cor06_svm()
-    mlModel.import_dataset("data/data-daily-ashaville-example.csv", 'csv')
-    print(mlModel.dfData.info())
+    mlModel.import_dataset("data/data-daily-asheville-example.csv", 'csv')
+    mlModel.getStandardDataTest()
+    mlModel.predictValues()
+    rmse, rrmse, mbe, r2, nse = mlModel.statAnalysis()
+    print(rmse, rrmse, mbe, r2, nse)
--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
 import unittest
-from cor06_svm import cor06_svm
 import os
+from cor06_svm import cor06_svm
 
 # go to root location
 os.chdir("..")
@@ -12,17 +12,42 @@ class TestCor06(unittest.TestCase):
         self.assertTrue(mlModel)
 
     def test_importDataset(self):
+        """
+        Check we correctly import data
+        """
         mlModel = cor06_svm()
-        mlModel.import_dataset("data/data-daily-ashaville-example.csv", 'csv')
-        self.assertNotEqual(mlModel.dfData.shape[0], 0, 'Error importing file')
+        mlModel.import_dataset("data/data-daily-asheville-example.csv", 'csv')
+
+        self.assertNotEqual(
+            first=mlModel.dfData.shape[0], second=0,
+            msg='Error importing file')
 
     def test_parameters(self):
+        """
+        Check the dataset contains all parameters
+        """
         mlModel = cor06_svm()
-        mlModel.import_dataset("data/data-daily-ashaville-example.csv", 'csv')
+        mlModel.import_dataset("data/data-daily-asheville-example.csv", 'csv')
         len_parameters = len(mlModel.parameters)
-        self.assertEqual(mlModel.dfData.shape[1], len_parameters)
 
+        self.assertEqual(
+            first=mlModel.dfData.shape[1], second=len_parameters,
+            msg="Error filtering the parameters from dataset")
 
+    def test_splitDataset(self):
+        mlModel = cor06_svm()
+        mlModel.import_dataset("data/data-daily-asheville-example.csv", 'csv')
+        x_test, y_test = mlModel.getStandardDataTest()
+
+        self.assertEqual(first=x_test.shape[0], second=y_test.shape[0])
+
+    def test_predictionValues(self):
+        mlModel = cor06_svm()
+        mlModel.import_dataset("data/data-daily-asheville-example.csv", 'csv')
+        mlModel.getStandardDataTest()
+        mlModel.predictValues()
+
+        self.assertEqual( mlModel.y_pred.shape[0], mlModel.x_test.shape[0])
 
 if __name__ == '__main__':
     unittest.main()
